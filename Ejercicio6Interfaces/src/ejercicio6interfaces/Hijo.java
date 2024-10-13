569c9b7ea092fafa4eafe52c65e11af99fc0585f
jhony chaves <developerjhony@gmail.com>
2024-10-13 12:45:24 +0200

    subida ejercicio 6 (por terminar)

---
+++ Ejercicio6Interfaces/src/ejercicio6interfaces/Hijo.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio6interfaces;
+
+/**
+ *
+ * @author Jhony
+ */
+public class Hijo {
+    
+    private String nombre;
+    private String Apellidos;
+    private String deporte;
+    private String nivel;
+    private String fechaNacimiento;
+
+    public Hijo(String nombre, String Apellidos, String deporte, String nivel, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.Apellidos = Apellidos;
+        this.deporte = deporte;
+        this.nivel = nivel;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public void setApellidos(String Apellidos) {
+        this.Apellidos = Apellidos;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    
+    
+}
